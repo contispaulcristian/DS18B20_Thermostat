d17be2bd134562d7338f70a19de572f01123971c
paul.contis <contispaulcristian@gmail.com>
2025-12-21 13:06:29 +0200

    Initial commit of DS18B20 Thermostat project

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * Thermostat.c
+ *
+ * Created: 12/21/2025 12:44:41 PM
+ * Author : conti
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
